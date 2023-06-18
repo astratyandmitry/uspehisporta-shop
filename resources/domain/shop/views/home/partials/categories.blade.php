08409f8a819607d78e24216fae038bbcea936be6
astratyandmitry <astratyandmitry@gmail.com>
2023-06-18 09:51:12 +0600

    Home banners

--- resources/domain/shop/views/home/partials/categories.blade.php
+++ resources/domain/shop/views/home/partials/categories.blade.php
@@ -5,34 +5,14 @@
     <div class="categories__content">
       <div class="categories__main">
         <a href="{{ route('shop::catalog', ['featured' => true]) }}" class="category category--main">
-          <img class="category-image" src="/images/man.png">
-
-          <div class="category-content">
-            <div class="category-label">
-              Только сегодня
-            </div>
-
-            <div class="category-name">
-              Акции дня
-            </div>
-          </div>
+          <img class="category-image" src="/images/home/sales.png">
         </a>
       </div>
 
       <div class="categories__grid">
         @foreach($layout->getCategories() as $category)
           <a href="{{ route('shop::catalog', $category) }}" class="category">
-            <img class="category-image" src="{{ $category->image }}" alt="{{ $category->title }}">
-
-            <div class="category-content">
-              <div class="category-label">
-                {{ $category->productsCount() }} товаров
-              </div>
-
-              <div class="category-name">
-                {{ $category->name }}
-              </div>
-            </div>
+            <img class="category-image" src="/images/home/{{ $category->hru }}.png" alt="{{ $category->title }}">
           </a>
         @endforeach
       </div>
