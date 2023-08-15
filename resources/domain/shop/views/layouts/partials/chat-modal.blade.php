f40eb008de3c386cb47b92d02fe5219dbdd3a7ad
astratyandmitry <astratyandmitry@gmail.com>
2023-08-15 13:16:50 +0600

    hotfix

--- resources/domain/shop/views/layouts/partials/chat-modal.blade.php
+++ resources/domain/shop/views/layouts/partials/chat-modal.blade.php
@@ -11,13 +11,13 @@
 
 
       <p>
-        <a href="https://t.me/uspehisportachat" target="_blank" class="i-button i-button--fill">
+        <a href="https://t.me/uspehisporta_news" target="_blank" class="i-button i-button--fill">
           Подписаться
         </a>
       </p>
 
       <p>
-        <span class="chat-message__close" x-on:click="closeModal(() => open = false)">Закрыть</span>
+        <button class="chat-message__close" x-on:click="closeModal(() => open = false)">Закрыть</button>
       </p>
     </div>
   </div>
@@ -26,7 +26,8 @@
 @push('scripts')
   <script src="https://cdn.jsdelivr.net/npm/alpinejs@2.8.2/dist/alpine.min.js" defer></script>
   <script>
-    function closeModal() {
+    function closeModal(callback) {
+      callback();
       localStorage.setItem('modalChatClosed', 'true');
     }
   </script>
