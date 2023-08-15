5c570d2fbd71b5369f6b4e144bc2a74b391dd1c0
astratyandmitry <astratyandmitry@gmail.com>
2023-08-15 10:31:28 +0600

    Features

--- resources/domain/shop/views/layouts/master.blade.php
+++ resources/domain/shop/views/layouts/master.blade.php
@@ -16,6 +16,8 @@
 </head>
 <body class="preload" tabindex="0">
 
+@include('shop::layouts.partials.chat-modal')
+
 <section id="shop" class="app">
   @include('shop::layouts.partials.nav')
 
@@ -34,7 +36,23 @@
   <div class="loader"></div>
 </section>
 
-
+<script id="chatBroEmbedCode">/* Chatbro Widget Embed Code Start */
+    function ChatbroLoader(chats, async) {
+        async = !1 !== async;
+        var params = {
+            embedChatsParameters: chats instanceof Array ? chats : [chats],
+            lang: navigator.language || navigator.userLanguage,
+            needLoadCode: 'undefined' == typeof Chatbro,
+            embedParamsVersion: localStorage.embedParamsVersion,
+            chatbroScriptVersion: localStorage.chatbroScriptVersion
+        }, xhr = new XMLHttpRequest;
+        xhr.withCredentials = !0, xhr.onload = function () {
+            eval(xhr.responseText)
+        }, xhr.onerror = function () {
+            console.error('Chatbro loading error')
+        }, xhr.open('GET', 'https://www.chatbro.com/embed.js?' + btoa(unescape(encodeURIComponent(JSON.stringify(params)))), async), xhr.send()
+    }/* Chatbro Widget Embed Code End */
+    ChatbroLoader({encodedChatId: '08wBX'});</script>
 <script src="{{ mix('/assets/shop/js/app.js') }}"></script>
 @stack('scripts')
 
---
+++ resources/domain/shop/views/layouts/partials/chat-modal.blade.php
@@ -0,0 +1,33 @@
+<div class="chat-modal" x-data="{open: true}" x-show="open && !localStorage.getItem('modalChatClosed')">
+  <div class="chat-modal__backdrop" x-on:click="closeModal(() => open = false)"></div>
+  <div class="chat-modal__content">
+    <div class="chat-message">
+      <h2>Важное сообщение</h2>
+
+      <p>
+        Друзья! Чтобы всегда быть в курсе новостей магазина и акций, просьба подписаться на наш Telegram канал. Также в
+        нем мы постоянно публикуем скидочные купоны и вы сможете покупать у нас еще дешевле! Заранее благодарим!
+      </p>
+
+
+      <p>
+        <a href="https://t.me/uspehisportachat" target="_blank" class="i-button i-button--fill">
+          Подписаться
+        </a>
+      </p>
+
+      <p>
+        <span class="chat-message__close" x-on:click="closeModal(() => open = false)">Закрыть</span>
+      </p>
+    </div>
+  </div>
+</div>
+
+@push('scripts')
+  <script src="https://cdn.jsdelivr.net/npm/alpinejs@2.8.2/dist/alpine.min.js" defer></script>
+  <script>
+    function closeModal() {
+      localStorage.setItem('modalChatClosed', 'true');
+    }
+  </script>
+@endpush
